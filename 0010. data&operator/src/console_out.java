b98894b6f68ddd00439211f69ecb2c091190579e
Fekim <81150979+97Fekim@users.noreply.github.com>
2021-12-05 22:43:20 +0900

    chapter2. data&operator
    int, float, char console_out & operator overview

---
+++ 0010. data&operator/src/console_out.java
@@ -0,0 +1,19 @@
+public class console_out 
+{
+	public static void main(String[] args) 
+	{
+		// char>>int console_out
+		System.out.printf("%1$d %2$d", (int)'3', (int)'A');
+		System.out.println();
+		// Variables
+		int nVar = 12;
+		float fVar = 3.14f;
+		System.out.print(nVar);
+		System.out.println();
+		System.out.print(fVar);
+		System.out.println();
+		System.out.println("정수 : " + nVar + "\n" + "부동소수 : " + fVar);
+		
+	}
+}
+
