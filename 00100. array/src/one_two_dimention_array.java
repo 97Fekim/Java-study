516d8723290be17c591aa8d8cdf412dd0f1a2cad
Fekim <81150979+97Fekim@users.noreply.github.com>
2021-12-08 21:43:05 +0900

    Array
    Array overview, 2 dimention array

---
+++ 00100. array/src/one_two_dimention_array.java
@@ -0,0 +1,46 @@
+
+public class one_two_dimention_array 
+{
+	public static void main(String[] args) 
+	{
+		// 참조형 배열변수
+		// int nArray[];
+		// int[] nArray;
+		// nArray = new int[7];
+		// int[] nArray = new int[3];
+		
+		
+		/* 배열 출력 */
+		/*int[] nArray;
+		nArray = new int[5];
+		
+		for(int i = 0; i < 5; i++)
+		{
+			nArray[i] = i + 1;
+			System.out.printf("%1$d\n", nArray[i]);
+		}*/
+		
+		
+		/* 자바만의 다차원 배열 */
+		/*int[][] nArray;
+		nArray = new int[3][];
+		nArray[0] = new int[2];
+		nArray[1] = new int[3];
+		nArray[2] = new int[5];
+		
+		for(int i = 0; i < nArray.length; i++)
+		{
+			for(int j = 0; j < nArray[i].length; j++)
+			{
+				nArray[i][j] = i * j;
+				System.out.printf("%d ",nArray[i][j]);
+			}
+			System.out.println();
+		}*/
+		
+		/* 선언과 동시에 초기화 */
+		/*int[][] nArray1 = {{1,2},{3,4}};
+		int[][] nArray2 = {{1,2},{3,4,5,6}};*/
+		
+	}
+}
