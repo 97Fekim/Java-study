a72c83a25dbe435770e6af3b06082a9d1becbe4c
Fekim <81150979+97Fekim@users.noreply.github.com>
2021-12-11 16:33:47 +0900

    package&import
    package&import example

---
+++ 00111. package_import/src/My_first_package/class_in_package.java
@@ -0,0 +1,9 @@
+package My_first_package;
+
+public class class_in_package {
+	public void Print()
+	{
+		System.out.println("my_first_package class");
+	}
+	
+}
---
+++ 00111. package_import/src/example.java
@@ -0,0 +1,12 @@
+import My_first_package.*;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		class_in_package exam = new class_in_package();
+		
+		exam.Print();
+	}
+
+}
